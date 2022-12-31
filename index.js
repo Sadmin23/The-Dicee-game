235fa4a9d815b017e93842faf8166ca501da60c6
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2022-12-31 11:42:30 +0600

    winner message added

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ var source = "images/dice" + randomNumber1 + ".png";
 
 document.querySelector(".img1").setAttribute("src",source);
 
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
 
-var source = "images/dice" + randomNumber1 + ".png";
+var source = "images/dice" + randomNumber2 + ".png";
 
 document.querySelector(".img2").setAttribute("src",source);
+
+if (randomNumber1>randomNumber2)
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+else if (randomNumber1===randomNumber2)
+    document.querySelector("h1").innerHTML = "Draw!";
+else
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩!";
